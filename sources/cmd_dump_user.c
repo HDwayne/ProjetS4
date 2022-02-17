9a96e19691aa5878015e2d041fd626c42c6b04f9
VerecundusSad <florianberlin7516@gmail.com>
2022-02-17 16:19:43 +0100

    changed cmd_dump_user function into a program

---
+++ sources/cmd_dump_user.c
@@ -0,0 +1,33 @@
+#include "../headers/layer3.h"
+
+int main(int argc, char **argv){
+    if (argc != 2){
+        fprintf(stderr, "Usage: %s <directory path>\n", argv[0]);
+        return 1;
+    }
+    init_disk_sos(directory);
+    update_first_free_byte();
+    printf("Superblock : %d %d %d %d\n", virtual_disk_sos->super_block.number_of_files, virtual_disk_sos->super_block.number_of_users, virtual_disk_sos->super_block.nb_blocks_used, virtual_disk_sos->super_block.first_free_byte);
+    init_user("root", "bonjour");
+    for (int i = 0; i < INODE_TABLE_SIZE; ++i) {
+        init_inode("bonjour.txt", 10, virtual_disk_sos->super_block.first_free_byte, timestamp(), timestamp());
+    }
+    delete_inode(4);
+    for (int i = 1; i < NB_USERS; ++i) {
+        init_user("Michel", "bonjour");
+    }
+    delete_user(3);
+    printf("Superblock : %d %d %d %d\n", virtual_disk_sos->super_block.number_of_files, virtual_disk_sos->super_block.number_of_users, virtual_disk_sos->super_block.nb_blocks_used, virtual_disk_sos->super_block.first_free_byte);
+    shutdown_disk_sos();
+    init_disk_sos(directory);
+    display_disk_storage();
+
+    for (int i = 0; i < virtual_disk_sos->super_block.number_of_users; ++i) {
+        fprintf(stdout, "userid %d: %s\n", i,virtual_disk_sos->users_table[i].login);
+        fprintf(stdout, "pass:%s\n", virtual_disk_sos->users_table[i].passwd);
+    }
+    printf("Superblock : %d %d %d %d\n", virtual_disk_sos->super_block.number_of_files, virtual_disk_sos->super_block.number_of_users, virtual_disk_sos->super_block.nb_blocks_used, virtual_disk_sos->super_block.first_free_byte);
+
+    shutdown_disk_sos();
+    return 0;
+}
--- sources/layer3.c
+++ sources/layer3.c
@@ -121,33 +121,7 @@ int init_user(char *login, char *password){
     return SUCCESS;
 }
 
-void cmd_dump_user(char *directory){
-    init_disk_sos(directory);
-    update_first_free_byte();
-    printf("Superblock : %d %d %d %d\n", virtual_disk_sos->super_block.number_of_files, virtual_disk_sos->super_block.number_of_users, virtual_disk_sos->super_block.nb_blocks_used, virtual_disk_sos->super_block.first_free_byte);
-    init_user("root", "bonjour");
-    for (int i = 0; i < INODE_TABLE_SIZE; ++i) {
-        init_inode("bonjour.txt", 10, virtual_disk_sos->super_block.first_free_byte, timestamp(), timestamp());
-    }
-    delete_inode(4);
-    for (int i = 1; i < NB_USERS; ++i) {
-        init_user("Michel", "bonjour");
-    }
-    delete_user(3);
-    printf("Superblock : %d %d %d %d\n", virtual_disk_sos->super_block.number_of_files, virtual_disk_sos->super_block.number_of_users, virtual_disk_sos->super_block.nb_blocks_used, virtual_disk_sos->super_block.first_free_byte);
-    shutdown_disk_sos();
-    init_disk_sos(directory);
-    display_disk_storage();
 
-    for (int i = 0; i < virtual_disk_sos->super_block.number_of_users; ++i) {
-        fprintf(stdout, "userid %d: %s\n", i,virtual_disk_sos->users_table[i].login);
-        fprintf(stdout, "pass:%s\n", virtual_disk_sos->users_table[i].passwd);
-    }
-    printf("Superblock : %d %d %d %d\n", virtual_disk_sos->super_block.number_of_files, virtual_disk_sos->super_block.number_of_users, virtual_disk_sos->super_block.nb_blocks_used, virtual_disk_sos->super_block.first_free_byte);
-
-    shutdown_disk_sos();
-
-}
 
 /**
  * @brief Look up the user's index in the table (if connected)
